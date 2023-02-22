1dc0e0856907dfdaa625c7f12079740c0afffcf6
oliviakovacs <kovacs.olivia.s@gmail.com>
2023-02-22 13:27:27 +0100

    Creating Components

---
+++ src/app/status/status.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  template: `
+    <h4>
+      Angular
+    </h4>
+  `,
+  styles: ["h4{color: blue}"
+  ]
+})
+export class StatusComponent {
+
+}
