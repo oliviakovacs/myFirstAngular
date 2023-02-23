6b2b2cd089c9e912e01a5ef2587d478a081b0848
oliviakovacs <kovacs.olivia.s@gmail.com>
2023-02-23 13:34:12 +0100

    Interpolation and Data Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor() {}
+    headerName = "Angular 14 tutorial";
+    salary = 1000;
+
+  ngOnInit(): void {
+    }
+
+  Functionclick(name:string){
+    alert(name)
+  }
 }
